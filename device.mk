aca0fcdc5c1c516f88347e83536ff9f2b40d3603
Zedisspp <juniordesigner077@gmail.com>
2026-06-06 13:16:41 -0300

    guam: add: Moto Camera 2 configuration to device.mk
    Credits
    https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotCamera2

--- device.mk
+++ device.mk
@@ -63,3 +63,9 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/motorola/guam/guam-vendor.mk)
+
+# Moto Camera 2
+TARGET_MOTCAMERA2 := whitney
+TARGET_USES_MOTCAMERA2 := true
+
+$(call inherit-product, vendor/motorola/MotCamera2/motcamera2.mk)
